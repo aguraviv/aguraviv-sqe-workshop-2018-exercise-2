4d452d0aebd679c05dac774a7a4e0f7ca62a0b2e
aguraviv <aguraviv12@gmail.com>
2018-12-17 13:53:55 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,24 @@
 import $ from 'jquery';
-import {parseCode, build_table, table_array} from './code-analyzer';
+import {parseCode, returnCode} from './code-analyzer';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
-        let parsedCode = parseCode(codeToParse);
-        $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
-        
-        //builds the new string to be added and sends it to the table
-        build_table(codeToParse);
+        let input_vactor = $('#input_vector').val();
+
+        parseCode(codeToParse,input_vactor);//builds the new string to be added and sends it to the table
         var string_to_insert;
-        for(var j=0; j<table_array.length; j++ ){
-            //console.log("table_array in the " + j + " line:" + table_array[j].line + "name: " +table_array[j].name );
-            var insert_to_table = the_string_to_add(table_array[j]);
+        var insert_to_table;
+        for(var j=0; j<returnCode.length; j++ ){
+            switch(returnCode[j].color){
+            case 2 : insert_to_table = '<span style="color: Green; ">' + returnCode[j].line + '</span><br>'; break;
+            case 3 : insert_to_table = '<span style="color: red; ">' + returnCode[j].line + '</span><br>'; break;
+            case 1 : insert_to_table = the_string_to_add(returnCode[j].line); 
+            }
             string_to_insert = string_to_insert + insert_to_table + ' ';
         }
-        $('#table_data').html(string_to_insert);
-    });
+        $('#table_data').html(string_to_insert); });
+        
 });
-
 //adds the string to be added
-const the_string_to_add = (the_new_line_to_string) => `<tr> <td> ${the_new_line_to_string.line} </td> <td>${the_new_line_to_string.type}</td> <td>${the_new_line_to_string.name}</td> <td>${the_new_line_to_string.condition}</td> <td>${the_new_line_to_string.value}`;   
+const the_string_to_add = (the_new_line_to_string) => `<tr> <td> ${the_new_line_to_string} `;
