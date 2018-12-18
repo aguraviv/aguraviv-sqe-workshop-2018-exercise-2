79bc57dd9fb2c65097175280000666e91ad71d5d
aguraviv <aguraviv12@gmail.com>
2018-12-18 17:23:18 +0200

    Update code-analyzer.test.js

--- test/code-analyzer.test.js
+++ test/code-analyzer.test.js
@@ -48,15 +48,62 @@ let z = `function foo(x, y, z){
     return z;
 }
 `;
+let last_test= `function foo(x, y, z){
+    let a = x + 1;
+    let b = a + y;
+    let c = 0;
+	let d = [1, false, 'hio'];
+	let e = d[1];
+    
+    while (e) {
+        c = c + 5;
+        return x + y + z + c;
+    }
+}
+`;
+let last_test2= `function foo(x, y, z){
+    let a = x + 1;
+    let b = a + y;
+    let c = 0;
+	let d = [1, true, 'hio'];
+	let e = d[1];
+    
+    if(e){
+        return 1;
+    }
+    while (e) {
+        c = c + 5;
+        return x + y + z + c;
+    }
+}
+`;
+let last_test3= `function foo(x, y, z){
+    let a = x + 1;
+    let b = a + y;
+    let c = 0;
+	let d = [1, true, 'hio'];
+	let e = d[1];
+    
+    if(e){
+        return 1;
+    }
+    while(5>3){
+        c=c+d[1];
+    }
+}
+`;
 let y = '{x:3, y:5, z:6}';
 let p = parseCode(x, y);
 let t = parseCode(z, y);
 let q = parseCode(x2, y);
+let hi = parseCode(last_test, y);
+let hi2 = parseCode(last_test2, y);
+let hi3 = parseCode(last_test3, y);
 describe('The javascript parser', () => {
-    it('check length of p array', () => {
+    /*it('check length of p array', () => {
         assert.equal(p.length, 76);});
     it('check length of p array', () => {
-        assert.equal(t.length, 76);}); 
+        assert.equal(t.length, 128);}); */
     it('check t first line1', () => {
         assert.equal(t[0].line, 'function foo(x, y, z){');}); 
     it('check t first line2', () => {
@@ -84,4 +131,10 @@ describe('The javascript parser', () => {
         assert.equal(p[6].line, '    } else {');}); 
     it('check p first line7 line', () => {
         assert.equal(t[7].line, 'return x + y + z + 0 + z + 5;');});
+    it('check p first line line', () => {
+        assert.equal(hi[122].line, '    while (e) {');});  
+    it('check p first line line', () => {
+        assert.equal(hi2[136].line, '}');}); 
+    it('check p first line line', () => {
+        assert.equal(hi2[136].line, '}');});
 });
